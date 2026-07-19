c43acac1201762cd4921c276102d077f87aa3153
miroyongCS <manuel0miro@gmail.com>
2026-07-19 19:48:15 -0400

    Update quotes.ts

--- data/quotes.ts
+++ data/quotes.ts
@@ -1,23 +1,45 @@
-export interface Quote {
+export type Quote = {
+  weekNumber: number;
   text: string;
-  author: string;
-}
+};
 
 export const quotes: Quote[] = [
   {
-    text: "O futebol é a coisa mais importante das coisas menos importantes da vida.",
-    author: "Pelé",
+    weekNumber: 3,
+    text: "Para a mais alta vitória, o mais profundo sacrifício.",
   },
   {
-    text: "Futebol é um esporte de equipe. O time, não o indivíduo, faz o sucesso.",
-    author: "Mia Hamm",
+    weekNumber: 4,
+    text: "Onde há uma vontade, sempre haverá um caminho.",
   },
   {
-    text: "A beleza do futebol está na sua simplicidade e na paixão que desperta.",
-    author: "Zinedine Zidane",
+    weekNumber: 5,
+    text:
+      "O shimjeong parental é tal que os pais desejam que seus filhos se tornem melhores do que eles próprios. Deus, o pai da humanidade, quer que os filhos que Ele criou se tornem melhores do que Ele mesmo.",
   },
   {
-    text: "Futebol não é sobre técnica. É sobre paixão.",
-    author: "Cristiano Ronaldo",
+    weekNumber: 6,
+    text:
+      "Se começarmos a entender a tristeza do Céu, não ficaremos deitados, mesmo que afundemos no chão.",
+  },
+  {
+    weekNumber: 7,
+    text:
+      "Quando você vive com um coração grato, tudo ao seu redor fica bonito. Isso o deixa feliz. Então, você não pode guardar isso para si mesmo e compartilha sua felicidade com os outros.",
+  },
+  {
+    weekNumber: 8,
+    text:
+      "Só podemos sentir alegria quando nossa dolorosa conexão de coração com Deus superar a que Adão e Eva tinham quando caíram.",
+  },
+  {
+    weekNumber: 9,
+    text:
+      "A família é o berço do verdadeiro amor, da verdadeira vida e da verdadeira linhagem, e o alicerce para a realização do propósito de criação de Deus.",
+  },
+  {
+    weekNumber: 10,
+    text:
+      "Nossa visão de paz é amor puro, famílias felizes e um mundo pacífico.",
   },
 ];
